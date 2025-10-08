d52412df9de4a372cd3e1f28d1172a77c44fd623
Elise Pletcher <59029338+epletcher@users.noreply.github.com>
2025-10-07 22:09:13 -0700

    updates

--- models/ModelPredictionsGrowth.R
+++ models/ModelPredictionsGrowth.R
@@ -1,6 +1,9 @@
 ## set working directory to 'PinonDemo'
 
 ## load model fitting workspace generated from 'ModelPreppingGrowth'
+# contains the fit 'growth_years_statespace.stan' growth model
+# for Bob and Elise this is located here: 
+# SEV_PJ_Demo/model_output_workspaces/growth_statespace_model.RData
 
 
 ## load packages
@@ -8,14 +11,15 @@ library(tidyverse)
 library(rstan)
 library(shinystan)
 
-## extract parameters
+## extract parameters from ss growth model
+# (no year effect here)
 
 growth.params.ss <- 
   as.matrix(growth_ss, pars = c("beta0","beta1","sigp","sigo")) %>% as.data.frame()
 
-Szl <- extract(growth_ss, pars = 'Szl')[[1]]
+Szl <- extract(growth_ss, pars = 'Szl')[[1]] # latent sizes
 
-## assign NA's to any latent values outside of the census endpoints
+## Re-assign NA's to any latent values outside of the census endpoints
 
 Szl.filt <- array(NA, dim = dim(Szl))
 
@@ -27,7 +31,7 @@ for(i in 1:length(tcy[,1])) { # for every tree
   
 }
 
-## extract median and upper and lower cis for the latent state
+## extract median and upper and lower cis for the latent state (for plotting)
 
 med.Szl <- apply(Szl.filt, MARGIN = c(2,3), FUN = median)
 
@@ -36,8 +40,8 @@ lo.Szl <- apply(Szl.filt, MARGIN = c(2,3), FUN = quantile, 0.05, na.rm = T)
 hi.Szl <- apply(Szl.filt, MARGIN = c(2,3), FUN = quantile, 0.95, na.rm = T)
 
 
-
-# ------- Explore estimated latent states ---------
+#
+# ------- Explore estimated size latent states ---------
 #plot latent state annual growth time series overlayed on top of observed annual growth time series (possibly for a subset of trees)
 
 # latent growth
@@ -59,25 +63,33 @@ lines(exp(med.Szl[,3]), lty = 2, col = 'purple')
 lines(exp(lo.Szl[,3]), lty = 2, col = 'purple')
 lines(exp(hi.Szl[,3]), lty = 2, col = 'purple')
 
+#
 # ------- GENERATE PREDICITONS ---------
-# Questions:
-# do i want to use the latent states here?
 
-# vector of Size_tmins for generating preds
-Sz.min.range <- seq(0.75,7,0.05)
+# Use latent states from fit model as size at t-1
+# Generate predictions for year effect growth model
+# (no year effect)
 
-## Generate predictions for year effect growth model
-# # Generate yhats for no year effect
-Sz.mean.pred.range <- matrix(NA,,)
+# empty array for predictions
+Sz.pred <- array(NA,c(dim(Szl.filt)[1],dim(Szl.filt)[2],dim(Szl.filt)[3]))
 
-  for(j in 1:dim(Sz)[2]) {
+# loop over iterations, and through years
+
+for(k in 1:dim(Szl.filt)[1]) {
+  
+  for(t in 2:dim(Szl.filt)[2]) { 
     
-    # start year that the tree first shows up (enters the census), end the year the tree dies (leaves the census for good), fill blank matrix for first year's value
-    for(t in (tcy[j,1]+1):tcy[j,2]) { 
-
-    Szl[t,j] ~ normal(beta0 + beta1*Szl[t-1,j], sigp);
+    # using latent size at t-1, and sigO to generate predictions here
+    Sz.pred[k,t,] <- rnorm(dim(Szl.filt)[3], exp(growth.params.ss$beta0[k] + growth.params.ss$beta1[k]*Szl.filt[k,t-1,]), growth.params.ss$sigo[k]) # eponentiate
     
-    Sz[t,j] ~ normal(exp(Szl[t,j]), sigo); # exponentiate here
+  }
   
 }
+
+# remove first year
+Sz.pred<-Sz.pred[,2:11,]    
+#
+# --------------- PLOT PREDICTIONS AGAINST LATENT STATES -----------
+
+plot(Sz.pred[50,,],exp(Szl.filt[50,2:11,])) # picking a random iteration
 
