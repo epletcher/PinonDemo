9d9efcc9dc011f2784a56768feb33192e51d889b
Elise Pletcher <59029338+epletcher@users.noreply.github.com>
2026-01-14 12:35:55 -0800

    updates

--- models/ModelPreppingReproduction.R
+++ models/ModelPreppingReproduction.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rstan)
 library(shinystan)
 library(cowplot)
+library(abind) # for combining arrays
 
 ## set working directory to 'PinonDemo' folder/repository
 
@@ -42,22 +43,24 @@ Sz.obs <- reprodat %>%
   select(-Year) %>%
   as.matrix()
 
+colnames(Sz.obs) <- NULL
 # Notes: some trees do not have height for 2024, so they will remain as NAs for heights back in time
 
+## Sz.obs.p is array of [year,individuals, all param iterations]
+
 # empty array to fill
-colnames(Sz.obs) <- NULL
 Sz.obs.p <- replicate(length(gp$beta0), Sz.obs)
 
-for (i in 1:length(gp$beta0)) { 
-  
-  for(t in dim(Sz.obs)[1]:2) { # back casting here, so descending order
-    
-    # trees are slowly shrinking?? check this equation
-    Sz.obs.p[t-1,,i] <- exp(rnorm(dim(Sz.obs.p)[2], (log(Sz.obs.p[t,,i])-gp$beta0[i])/gp$beta1[i], gp$sigp[i])) # inlcude process error because we become less certain of tree size further into the past
-    
-  }
-  
-}
+ for (i in 1:length(gp$beta0)) {
+
+   for(t in dim(Sz.obs)[1]:2) { # back casting here, so descending order
+
+     # trees are slowly shrinking?? check this equation
+     Sz.obs.p[t-1,,i] <- exp(rnorm(dim(Sz.obs.p)[2], (log(Sz.obs.p[t,,i])-gp$beta0[i])/gp$beta1[i], gp$sigp[i])) # inlcude process error because we become less certain of tree size further into the past
+
+   }
+
+ }
 
 # Visual check of back casted tree heights 
 matplot(Sz.obs.p[,35,], type = "l") # plot for a particular tree
@@ -66,10 +69,45 @@ matplot(Sz.obs.p[,35,], type = "l") # plot for a particular tree
 thin.iter <- seq(9,dim(Sz.obs.p)[3], by = 9) # vector of every 10th iteration
 
 # Sz <- log(Sz.obs.p[,,thin.iter]) # model SZ on the log scale
-Sz <- Sz.obs.p[,,thin.iter] # model SZ w/o log scale
+# Sz <- Sz.obs.p[,,thin.iter] # model SZ w/o log scale
 
-# Visual check of *thinned* back casted tree heights 
-matplot(Sz[,35,], type = "l") # plot for a particular tree
+# # Visual check of *thinned* back casted tree heights 
+# matplot(Sz[,35,], type = "l") # plot for a particular tree
+
+## TO SHARE: 
+
+#a single tree example, all iterations, all years
+matplot(Sz.obs.p[,35,], type = "l", lty = 1 , xlab = 'year', ylab = 'height (m)', col = rgb(red = 0, green = 0.5, blue = 0.6, alpha = 0.05)) 
+# mean
+lines(apply(Sz.obs.p, MARGIN = c(1,2), FUN = mean)[,35], lwd = 2.5)
+
+#all trees mean
+matplot(apply(Sz.obs.p, MARGIN = c(1,2), FUN = mean), type = "l", lty = 1 , xlab = 'year', ylab = 'height (m)')
+
+
+## UPDATE FOR IMPROVE MODEL CONVERGENCE:
+## Simplify Sz.obs.p so that it is an array of [year,individuals, mean, lo and hi CI's from growth model]
+
+# low CI/ high growth scenario
+Sz.obs.loci <- apply(Sz.obs.p, MARGIN = c(1,2), FUN = quantile, 0.05, na.rm = T)
+
+# mean growth CI / average growth scenario
+Sz.obs.avg <- apply(Sz.obs.p, MARGIN = c(1,2), FUN = mean, na.rm = T)
+
+# hi growth CI / low growth scenario
+Sz.obs.upci <- apply(Sz.obs.p, MARGIN = c(1,2), FUN = quantile, 0.95, na.rm = T)
+
+# combine
+Sz.obs.p3 <- abind(Sz.obs.upci, Sz.obs.avg, Sz.obs.loci, along = 3)
+
+#a single tree example, low mean and hi growth, all years
+matplot(Sz.obs.p3[,15,], type = "l", lty = 1 , xlab = 'year', ylab = 'height (m)', col = c("dodgerblue", "black", "coral")) 
+
+#all trees mean
+matplot(Sz.obs.p3[,,2], type = "l", lty = 1 , xlab = 'year', ylab = 'height (m)')
+
+#
+Sz <- Sz.obs.p3 # for scenarios based model, mode SZ w/o log scale for now
 
 # ------- FIT REPRODUCTION MODEL IN STAN -----
 ## prep size data for STAN
@@ -77,31 +115,45 @@ matplot(Sz[,35,], type = "l") # plot for a particular tree
 # dimensions
 i = dim(Sz)[2] # individual
 y = dim(Sz)[1] # year
-k = dim(Sz)[3] # growth model iterations
+k = dim(Sz)[3] # growth model iterations OR growth scenarios (low, avg , high)
 
-# id for year effect
-yr <- unique(reprodat$Year)[3:27]
+# # id for year effect
+# yr <- unique(reprodat$Year)[3:27]
 
 # specify model data
 # reprodata <- list(i=i,k=k,y=y,yr=yr,Sz=Sz,cp=cp) # year fixed effect
 
 reprodata <- list(i=i,k=k,y=y,Sz=Sz,cp=cp) # year random effect
 
-# set initial true size as the mean across param/process uncertainty values
-start <- list(list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)),
-              list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)),
-              list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)))
-
-
 # fit reproduction model
-options(mc.cores = parallel::detectCores())
-reprofit1 <- stan(file='models/reproduction.stan', data=reprodata, init = start, chains=3, iter=1000, warmup=500) # increase iterations later but need to debug
 
-reprofit1 
+## state space fitting
+# # set initial true size as the mean across param/process uncertainty values
+# start <- list(list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)),
+#               list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)),
+#               list("tsz"=apply(Sz, MARGIN = c(1,2), FUN = mean)))
 
-launch_shinystan(reprofit1)
+# options(mc.cores = parallel::detectCores())
+# reprofit1 <- stan(file='models/reproduction.stan', data=reprodata, init = start, chains=3, iter=1000, warmup=500) # increase iterations later but need to debug
+# 
+# reprofit1 
+# 
+# launch_shinystan(reprofit1)
 
 # ** issues, fitting latent 25 okay, but then bad for all other years, sigs trails off
+
+## scenario based model fitting
+# set initial true size as the mean across param/process uncertainty values
+start <- list(list("alpha1"=rep(0.01,y),"alpha2"=rep(0.01,y),"alpha3"=rep(0.01,y),"beta1"=rep(1,y),"beta2"=rep(1,y),"beta3"=rep(1,y)),
+              list("alpha1"=rep(0.01,y),"alpha2"=rep(0.01,y),"alpha3"=rep(0.01,y),"beta1"=rep(1,y),"beta2"=rep(1,y),"beta3"=rep(1,y)),
+              list("alpha1"=rep(0.01,y),"alpha2"=rep(0.01,y),"alpha3"=rep(0.01,y),"beta1"=rep(1,y),"beta2"=rep(1,y),"beta3"=rep(1,y)))
+
+options(mc.cores = parallel::detectCores())
+reprofit2 <- stan(file='models/reproductionv2.stan', data=reprodata, chains=3, init=start, iter=1000, warmup=500) # increase iterations later but need to debug
+
+reprofit2
+
+launch_shinystan(reprofit2)
 
 # -------- Extract posterior estimates ------
 # put parameter estimates in a dataframe
--- models/reproductionv2.stan
+++ models/reproductionv2.stan
@@ -10,61 +10,6 @@ data {
   
 }
 
-// MODEL 1: state space model using parameter uncertainty from the growth model
-
-// // The parameters accepted by the model. Our model
-// parameters {
-//   real beta0[y];
-//   real beta1[y];
-//   real<lower = 0> sigs[y]; // by year, because variances it grows back in time
-//   
-//   real beta0mu;
-//   real beta1mu;
-//   real sigmu;
-//   real<lower = 0> tausq0;
-//   real<lower = 0> tausq1;
-//   real<lower = 0> tausqs;
-//   
-//   matrix[y,i] tsz; //latent true size at t for individual i
-//   
-// }
-// 
-// model {
-//       
-//         for(j in 1:i){ // individuals
-//           
-//           for(t in 1:y) { // years
-//             
-//             if(cp[t,j]!=999) { // in order to skip over NA's
-//           
-//     //Sz[t,j,] ~ normal(tsz[t,j], sigs[t])T[0,]; 
-//     Sz[t,j,] ~ normal(tsz[t,j], sigs[t]); // is the issue that we are forcing this to be negative when logged?? trying w/o truncation
-//     
-//     cp[t,j] ~ poisson(exp(beta0[t] + beta1[t]*exp(tsz[t,j]))); // exponentiate logged size
-//     
-//     //cp[t,j] ~ poisson(exp(beta0[t] + beta1[t]*tsz[t,j])); // unlogged size
-//           }
-//             
-//        }
-//     
-//     }
-//   
-//   //priors
-//   beta0 ~ normal(beta0mu,tausq0); 
-//   beta0mu ~ normal(0,10);
-//   tausq0 ~ normal(0,10)T[0,];
-//   
-//   beta1 ~ normal(beta1mu,tausq1);
-//   beta1mu ~ normal(1,10);
-//   tausq1 ~ normal(0,10)T[0,];
-//   
-//   // sigs ~ inv_gamma(1,1); // try sigs as fixed effect
-//   sigs ~ normal(sigmu,tausqs)T[0,];
-//   sigmu ~ normal(0,0.05); // (0,0.5) = no log; (0,0.05) = logged version , still issues w 0.05, try 0.01
-//   tausqs ~ normal(0,1)T[0,]; // also trying 1 instead of 10 here
-//   
-//   }
-//   
 
 // MODEL 2: No state space model, modeling reproduction under 3 growth scenarios (low, average, high)
 
