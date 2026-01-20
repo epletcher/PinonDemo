28e9e5302c887b5416e225435e370344707ce7d8
Elise Pletcher <59029338+epletcher@users.noreply.github.com>
2026-01-20 10:25:20 -0800

    Update ModelPredictionsReproduction.R

--- models/ModelPredictionsReproduction.R
+++ models/ModelPredictionsReproduction.R
@@ -8,7 +8,7 @@ library(LaplacesDemon)
 
 ## extract reproduction model params from fit model
 repro.params <- 
-  as.matrix(reprofit2) %>% as.data.frame()
+  as.matrix(reprofit3) %>% as.data.frame()
 
 ## Convert alpha's and beta's from reproduction models into matrices
 alpha1 <- repro.params %>% select(starts_with('alpha1[')) %>% as.matrix()
@@ -36,10 +36,14 @@ hg.cone.pred <- array(NA,c(length(Sz.range),y,length(alpha3[,1])))
 for (k in 1:length(alpha1[,1])) {
   
   for(t in 1:y) {
-  
-  lg.cone.pred[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz.range + beta1_2[k,t]*Sz.range^2)
-  mg.cone.pred[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz.range + beta2_2[k,t]*Sz.range^2)
-  hg.cone.pred[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz.range + beta3_2[k,t]*Sz.range^2)
+  # quad
+  # lg.cone.pred[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz.range + beta1_2[k,t]*Sz.range^2)
+  # mg.cone.pred[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz.range + beta2_2[k,t]*Sz.range^2)
+  # hg.cone.pred[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz.range + beta3_2[k,t]*Sz.range^2)
+  # no quad
+  lg.cone.pred[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz.range)
+  mg.cone.pred[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz.range)
+  hg.cone.pred[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz.range)
   
   }
   
@@ -51,7 +55,7 @@ matplot(apply(mg.cone.pred, MARGIN = c(1,2), FUN = mean), type = "l")
 matplot(apply(hg.cone.pred, MARGIN = c(1,2), FUN = mean), type = "l")
 
 # ----------- Posterior predictive check -------------
-# **** editing here
+
 # generate predicted cone production using observed sizes
 cp.yhat.lg <- array(NA,c(length(cp.obs[1,]),y,length(alpha1[,1])))
 cp.lambda.lg <- array(NA,c(length(cp.obs[1,]),y,length(alpha1[,1])))
@@ -63,15 +67,25 @@ cp.lambda.hg <- array(NA,c(length(cp.obs[1,]),y,length(alpha1[,1])))
 for (k in 1:length(alpha1[,1])) {
   
   for(t in 1:y) {
+    # quad
+    # cp.yhat.lg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha1[k,t] + beta1[k,t]*Sz[t,,1] + beta1_2[k,t]*Sz[t,,1]^2))
+    # cp.lambda.lg[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz[t,,1] + beta1_2[k,t]*Sz[t,,1]^2)
+    # 
+    # cp.yhat.mg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha2[k,t] + beta2[k,t]*Sz[t,,2] + beta2_2[k,t]*Sz[t,,2]^2))
+    # cp.lambda.mg[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz[t,,2] + beta2_2[k,t]*Sz[t,,2]^2)
+    # 
+    # cp.yhat.hg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3] + beta3_2[k,t]*Sz[t,,3]^2))
+    # cp.lambda.hg[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3] + beta3_2[k,t]*Sz[t,,3]^2)
     
-    cp.yhat.lg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha1[k,t] + beta1[k,t]*Sz[t,,1] + beta1_2[k,t]*Sz[t,,1]^2))
-    cp.lambda.lg[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz[t,,1] + beta1_2[k,t]*Sz[t,,1]^2)
+    # no quad
+    cp.yhat.lg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha1[k,t]))
+    cp.lambda.lg[,t,k] <- exp(alpha1[k,t] + beta1[k,t]*Sz[t,,1])
     
-    cp.yhat.mg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha2[k,t] + beta2[k,t]*Sz[t,,2] + beta2_2[k,t]*Sz[t,,2]^2))
-    cp.lambda.mg[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz[t,,2] + beta2_2[k,t]*Sz[t,,2]^2)
+    cp.yhat.mg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha2[k,t]))
+    cp.lambda.mg[,t,k] <- exp(alpha2[k,t] + beta2[k,t]*Sz[t,,2])
     
-    cp.yhat.hg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3] + beta3_2[k,t]*Sz[t,,3]^2))
-    cp.lambda.hg[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3] + beta3_2[k,t]*Sz[t,,3]^2)
+    cp.yhat.hg[,t,k] <- rpois(length(cp.obs[1,]), exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3]))
+    cp.lambda.hg[,t,k] <- exp(alpha3[k,t] + beta3[k,t]*Sz[t,,3])
     
   }
   
@@ -122,12 +136,12 @@ pval.lg/length(alpha1[,1])
 pval.mg/length(alpha1[,1])
 pval.hg/length(alpha1[,1])
 
-hist(devobs.repro.lg, col=rgb(0,0,1,1/4))  
-hist(devsim.repro.lg, col=rgb(1,0,0,1/4))  
+hist(devobs.repro.lg, col=rgb(0,0,1,1/4), xlim = c(25000,500000))  
+hist(devsim.repro.lg, col=rgb(1,0,0,1/4), add = T)  
 
-hist(devobs.repro.mg, col=rgb(0,0,1,1/4))
-hist(devsim.repro.lg, col=rgb(1,0,0,1/4))
+hist(devobs.repro.mg, col=rgb(0,0,1,1/4), xlim = c(25000,500000))
+hist(devsim.repro.lg, col=rgb(1,0,0,1/4), add = T)
 
-hist(devobs.repro.hg, col=rgb(0,0,1,1/4))
-hist(devsim.repro.lg, col=rgb(1,0,0,1/4))
+hist(devobs.repro.hg, col=rgb(0,0,1,1/4), xlim = c(25000,500000))
+hist(devsim.repro.lg, col=rgb(1,0,0,1/4), add = T)
 
